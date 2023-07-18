ead08458c158a460183906eafa338a6402044496
rfinkbeiner <135240688+rfinkbeiner@users.noreply.github.com>
2023-07-18 16:33:49 +0200

    Create flake.nix

---
+++ solar_wind/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Nix-flake-based development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      overlays = [
+        (final: prev: rec {
+          mermaid = prev.nodePackages.mermaid-cli;
+        })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ mermaid ];
+        };
+      });
+    };
+}
